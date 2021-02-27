4de45efe41d6f2a4af60ef4ebce74bd2a7a0bea8
coltman151 <65498634+coltman151@users.noreply.github.com>
2021-02-27 17:21:44 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def select_parameter(conn, parameter):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM param_xref where PF40_Param=? COLLATE NOCASE", (parameter,))
+
+    rows = cur.fetchall()
+    #print(rows)
+
+    pf525 = [x[2] for x in rows]
+    desc = [x[1] for x in rows]
+    note = [x[3] for x in rows]
+    print(f'PF525 Parameter for {desc[0]}is {pf525[0]}')
+    if note[0] != '':
+        print(f"Note: {note[0]} ")
+
+def main():
+    database = "param_xref.db"
+
+    conn = create_connection(database)
+    with conn:
+        parameter = input("Enter the PF40 Parameter Number: ")
+        select_parameter(conn, parameter)
+
+
+if __name__ == '__main__':
+    main()
