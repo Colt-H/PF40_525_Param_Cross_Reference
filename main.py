20fdbbe47651841c527ce663ffbe4ca5b6be7915
coltman151 <65498634+coltman151@users.noreply.github.com>
2021-02-28 07:57:06 -0500

    gitignore, add error handling for missing params

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
+    except Error as return_error:
+        print(return_error)
 
     return conn
 
@@ -15,22 +15,26 @@ def select_parameter(conn, parameter):
     cur.execute("SELECT * FROM param_xref where PF40_Param=? COLLATE NOCASE", (parameter,))
 
     rows = cur.fetchall()
-    #print(rows)
 
-    pf525 = [x[2] for x in rows]
-    desc = [x[1] for x in rows]
-    note = [x[3] for x in rows]
-    print(f'PF525 Parameter for {desc[0]}is {pf525[0]}')
-    if note[0] != '':
-        print(f"Note: {note[0]} ")
+    try:
+        pf525 = [x[2] for x in rows]
+        desc = [x[1] for x in rows]
+        note = [x[3] for x in rows]
+        print(f'PF525 Parameter for {desc[0]}is {pf525[0]}')
+        if note[0] != '':
+            print(f"Note: {note[0]} ")
+    except IndexError:
+        print("No Parameter Returned")
+
 
 def main():
-    database = "param_xref.db"
+    while True:
+        database = "param_xref.db"
 
-    conn = create_connection(database)
-    with conn:
-        parameter = input("Enter the PF40 Parameter Number: ")
-        select_parameter(conn, parameter)
+        conn = create_connection(database)
+        with conn:
+            parameter = input("Enter the PF40 Parameter Number: ")
+            select_parameter(conn, parameter)
 
 
 if __name__ == '__main__':
